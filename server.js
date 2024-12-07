6bf69f170aa014fbcc4aed929e416306d6163adf
Pedro Blanco <ppblanco.jaramillo@yahoo.com>
2024-12-07 14:52:41 -0800

    Fixed the HTML and server

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const socketio = require('socket.io')
-var express = require('express')
-var path = require('path')
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+const path = require('path')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+const PORT = process.env.PORT || 3001
 
 var expressHandlebars = require('express-handlebars');
 var userData = require("./client_files/userData.json");
-
-
-const app = express()
-var port = process.env.PORT || 3001
 
 // Set Handlebars as the view engine
 app.engine('handlebars', expressHandlebars.engine());
@@ -15,8 +18,11 @@ app.set('view engine', 'handlebars');
 // Set the views directory
 app.set('views', path.join(__dirname, 'client_files/views'));
 
-
 app.use(express.static(path.join(__dirname, 'client_files')))
+
+io.on('connection', (socket) => {
+    console.log("User has connected:", socket.id)
+})
 
 app.get('/', function (req, res) {
     //res.sendFile(path.join(__dirname, 'client_files', 'game.html'))
@@ -24,6 +30,6 @@ app.get('/', function (req, res) {
 
 })
 
-app.listen(port, function() {
-    console.log("Server listening at port", port)
+server.listen(PORT, function() {
+    console.log("Server listening at port", PORT)
 })
